cf31bf335fb06270a4f9b58f486349f98c70567e
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2023-12-11 11:24:12 +0200

    fix LS

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,29 +5,19 @@ import { SearchFilter } from './SearchFilter/SearchFilter';
 import { useEffect, useState } from 'react';
 
 export const App =() => {
-  
-  const [contacts, setContacts] = useState([
+
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) ?? [
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
   ])
   const [filter, setFilter] = useState('');
-  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
-    const storedContacts = localStorage.getItem('contacts');
-    if (storedContacts && !loaded) {
-      setContacts(JSON.parse(storedContacts));
-      setLoaded(true);
-    }
-  }, [loaded]);
- 
-  useEffect(() => {
-    if (loaded) {
       localStorage.setItem('contacts', JSON.stringify(contacts));
-    }
-  }, [contacts, loaded]);
+  }, [contacts]);
 
 const  deleteContact = (id) => {
     setContacts(prevState => (prevState.filter(contact => contact.id !== id)))
